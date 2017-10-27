a392b4ecac1d53eb3bb39001df17177b197c7350
qiwx <qiwx@jingzhengu.com>
2017-10-27 20:18:45 +0800

    修改返回类型

--- src/com/qiwx/action/LoginAction.java
+++ src/com/qiwx/action/LoginAction.java
@@ -11,13 +11,14 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import com.google.gson.Gson;
+import com.qiwx.ResponseJson;
 import com.qiwx.UserInfo;
 import com.qiwx.form.LoginForm;
 
 /**
- *@Author qiwx
- *@time  2017年7月4日 下午6:03:12
- *@Des
+ * @Author qiwx
+ * @time 2017年7月4日 下午6:03:12
+ * @Des
  **/
 public class LoginAction extends Action {
 
@@ -26,29 +27,31 @@ public class LoginAction extends Action {
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 		System.out.println("execute======");
-		LoginForm lfForm=(LoginForm)form;
-		String userNameString=lfForm.getUsername();
-		String pwdString=lfForm.getPassword();
-		if(userNameString!=null&&userNameString.equals("qiwx")){
-		      //request.getRequestDispatcher("/Success.jsp").forward(request, response);
-			//return mapping.findForward("success");
+		LoginForm lfForm = (LoginForm) form;
+		String userNameString = lfForm.getUsername();
+		String pwdString = lfForm.getPassword();
+		if (userNameString != null && userNameString.equals("qiwx")) {
+			// request.getRequestDispatcher("/Success.jsp").forward(request,
+			// response);
+			// return mapping.findForward("success");
 			response.setContentType("application/json; charset=utf-8");
 			response.setCharacterEncoding("UTF-8");
-			/*UserInfo userInfo = new UserInfo();
+			UserInfo userInfo = new UserInfo();
 			userInfo.setUserName("小明");
 			userInfo.setPassWord("123456");
-			String userJson = new Gson().toJson(userInfo);*/
-			String userJson="adfasdfasfasfasdfa";
+			ResponseJson rjJson = new ResponseJson<UserInfo>();
+			rjJson.setData(userInfo);
+			rjJson.setStatus(200);
+			rjJson.setMessage("请求成功");
+			String userJson = new Gson().toJson(rjJson);
 			OutputStream out = response.getOutputStream();
 			out.write(userJson.getBytes("UTF-8"));
 			out.flush();
-		}else{
-			//return mapping.findForward("fail");
+		} else {
+			// return mapping.findForward("fail");
 		}
-		
+
 		return null;
 	}
-	
-	
 
 }
